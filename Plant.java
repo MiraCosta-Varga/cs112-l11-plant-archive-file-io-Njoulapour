15987bc4c20b88ae3bb03d69abc9a787d4ac4bc3
Veronika Joulapour <nika.j2016@yahoo.com>
2024-11-25 11:08:26 -0800

    Send

--- Plant.java
+++ Plant.java
@@ -34,7 +34,33 @@ public class Plant {
 	}
 
 	//TODO: Step 1 = CSV string constructor
+public Plant(String csv) {
+		if(csv == null || csv.length() == 0) {
+			throw new
+					IllegalArgumentException("No CSV string given to constructor");
+		}
+		String[] parts = csv.split(",");
 
+		if(parts.length !=3) {
+			throw new IllegalArgumentException("CSV string doesn't have required number of values, csv String = " + csv);
+		}
+
+	String name = parts[0];
+		double temp;
+		try {
+			temp = Double.parseDouble(parts[1]);
+		} catch(NumberFormatException nfe) {
+			throw new
+					IllegalArgumentException("CSV string doesn't have a valid temp (double) as second value in csv string. second value = " + parts[1]);
+		}
+
+
+		String uses = parts[2];
+
+	if(!this.setAll(name,temp,uses)) {
+		throw new IllegalArgumentException("Invalid datat provided after parsing, name=" + name + ", temp=" + temp + ", uses=" + uses);
+	}
+}
 
 
 
